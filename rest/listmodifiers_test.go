e9a46026405481e740acf00a9aa70233456117e0
Vincent Serpoul <vincent@serpoul.com>
2019-03-11 00:24:49 +0800

    add test for fromURL

---
+++ rest/listmodifiers_test.go
@@ -0,0 +1,42 @@
+package rest
+
+import (
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/induzo/crud"
+)
+
+func TestListModifiersFromURL(t *testing.T) {
+	type args struct {
+	}
+	tests := []struct {
+		name string
+		url  string
+		want crud.ListModifiers
+	}{
+		{
+			name: "different querystring params",
+			url:  "http://test.com?premier=1",
+			want: crud.ListModifiers{
+				"premier": []string{"1"},
+			},
+		},
+		{
+			name: "same querystring param name",
+			url:  "http://test.com?orderby=test%20ASC&orderby=pol%20DESC",
+			want: crud.ListModifiers{
+				"orderby": []string{"test ASC", "pol DESC"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u, _ := url.Parse(tt.url)
+			if got := ListModifiersFromURL(u); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ListModifiersFromURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
